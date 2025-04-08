9942beac7cedb72607fe498161dba82fc6b95e0a
felxx <igorfelipemonteirocontato@gmail.com>
2025-04-07 22:24:19 -0300

    feat(formulario): formulário de contato implementado

---
+++ js/script.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+    construirModal();
+});
+
+function construirModal() {
+    const botaoEmail = document.getElementById("botao-email");
+    const modal = document.getElementById("modal");
+    const botaoFechar = document.getElementById("fechar-modal");
+
+    botaoFechar.addEventListener("click", () => {
+        modal.classList.add("hidden");
+    });
+
+    botaoEmail.addEventListener("click", () => {
+        modal.classList.remove("hidden");
+    });
+
+    window.addEventListener("click", (e) => {
+        console.log(e.target);
+        if (e.target == modal) {
+            modal.classList.add("hidden");
+        }
+    });
+}
