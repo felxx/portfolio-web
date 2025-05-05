eab404dea7e3d45218c9e91eb7f95da5db6608c2
felxx <igorfelipemonteirocontato@gmail.com>
2025-05-05 15:33:14 -0300

    site bilíngue implementado

--- js/script.js
+++ js/script.js
@@ -26,3 +26,28 @@ function construirModal() {
 var radio = document.querySelector('.nav-botao')
 
 document.getElementById('slide-1').checked = true;
+
+let idiomaAtual = "pt";
+
+function alterarIdioma() {
+    idiomaAtual = idiomaAtual == "pt" ? "en" : "pt";
+    carregarIdioma(idiomaAtual);
+}
+
+function carregarIdioma(idioma) {
+    fetch(`json/${idioma}.json`)
+        .then(data => data.json())
+        .then(data => {
+            traduzirPagina(data);
+        });
+}
+
+function traduzirPagina(linguagem) {
+    document.querySelectorAll("[data-i18n]").forEach(elemento => {
+        console.log(elemento);
+        const chave = elemento.getAttribute("data-i18n");
+        console.log(chave);
+        if (linguagem[chave]) {
+            elemento.textContent = linguagem[chave];
+        }
+    })};
